9d392fbba9bd8faec97d4f674d62f2dd096e5c83
Himanshu <himvarandani@gmail.com>
2022-05-28 06:46:23 +0530

    Update UI and add login feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import { AuthProvider } from "./context";
 import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
 
 const App = () => {
   return (
     <AuthProvider>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </AuthProvider>
   )
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,18 @@
+
+const Navbar = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      padding: '10px 60px',
+      backgroundColor: '#161b22cc'
+    }}>
+      <h1 style={{
+        color: "white",
+      }}>
+        CrossX
+      </h1>
+    </div>
+  )
+}
+
+export default Navbar
